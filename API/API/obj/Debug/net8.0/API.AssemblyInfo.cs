3938f5de675bfb031af1e5f0c039f06ef29f4746
malobr <marcelotc1202@gmail.com>
2024-05-04 21:58:16 -0300

    teste

--- API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ea65904075da6f48ceac6a201e6239299b65ba4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00caa4ad00d282d3281287d3373a1cf4f85a0bf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
