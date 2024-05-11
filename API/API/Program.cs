7df3ddf128935e5ff7125789628e123f7ca63ab9
malobr <marcelotc1202@gmail.com>
2024-05-11 15:49:45 -0300

    Cadastro e listagem adicionados ao .http

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDataContext>();
+builder.Services.AddControllers();
 var app = builder.Build();
 
 app.MapGet("/", () => "API Gerenciadora de Balada");
@@ -67,6 +68,83 @@ app.MapPost("/eventos/cadastrar", ([FromBody] Eventos e, [FromServices] AppDataC
         return Results.Created("", e);
     }
     return Results.BadRequest("Este Evento já está no sistema!");
+
+});
+
+
+
+
+// GET: http://localhost:5096/cliente/listar
+app.MapGet("/cliente/listar", ([FromServices] AppDataContext ctx) => {
+
+    if (ctx.tabClientes.Any())
+    {
+        return Results.Ok(ctx.tabClientes.ToList());
+    }
+        return Results.BadRequest("No momento, não há clientes no sistema...");
+});
+
+
+// GET: http://localhost:5096/funcionario/listar
+app.MapGet("/funcionario/listar", ([FromServices] AppDataContext ctx) => {
+
+    if (ctx.tabFuncionarios.Any())
+    {
+        return Results.Ok(ctx.tabFuncionarios.ToList());
+    }
+        return Results.BadRequest("Sem funcionários cadastrados no momento...");
+});
+
+// GET: http://localhost:5096/eventos/listar
+app.MapGet("/eventos/listar", ([FromServices] AppDataContext ctx) => {
+
+    if (ctx.tabEventos.Any())
+    {
+        return Results.Ok(ctx.tabEventos.ToList());
+    }
+        return Results.BadRequest("Aguardando eventos...");
+});
+
+
+
+
+//Exclusão de clientes, funcionarios e eventos
+app.MapDelete("/cliente/excluir/{id}", ([FromRoute] string id, [FromServices] AppDataContext ctx) => {
+
+    Cliente? c = ctx.tabClientes.FirstOrDefault(x => x.Id == id);
+    if (c is null)
+    {
+        return Results.NotFound("Cliente não encontrado!");
+    }
+    ctx.tabClientes.Remove(c);
+    ctx.SaveChanges();
+    return Results.Ok("Cliente deletado!");
+
+});
+
+//
+app.MapDelete("/funcionario/excluir/{id}", ([FromRoute] string id, [FromServices] AppDataContext ctx) => {
+
+    Funcionario? f = ctx.tabFuncionarios.FirstOrDefault(x => x.Id == id);
+        if (f is null)
+        {
+            return Results.NotFound("Funcionário não encontrado!");
+        }
+        ctx.tabFuncionarios.Remove(f);
+        ctx.SaveChanges();
+        return Results.Ok("Funcionário deletado!");
+});
+
+//
+app.MapDelete("/eventos/excluir/{id}", ([FromRoute] string id, [FromServices] AppDataContext ctx) => {
+
+    Eventos? e = ctx.tabEventos.FirstOrDefault(x => x.Id == id);
+    if(e is null){
+        return Results.NotFound("Evento não encontrado!");
+    }
+    ctx.tabEventos.Remove(e);
+    ctx.SaveChanges();
+    return Results.Ok("Evento deletado!");
 
 });
 
