f2347b4d1d7dae658bc84c8866eadafcecd1f076
Brian Snack <snackbrian10@gmail.com>
2024-05-15 17:41:21 -0300

    Alteração nos dados das tabelas no banco de dados

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9d9ce4fdc4b02d52580c5f2e306362ff4ab17b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db025432193411b2b914d4b647c99c5265c3f00c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
